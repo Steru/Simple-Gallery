b285a976a716967ae716cff40df74277341dc4e6
Steru <s.stergiadis@gmail.com>
2016-06-28 01:10:12 +0200

    first square recyclerView grid test

--- app/src/main/java/com/stergiadis/simplegallery/GridRecyclerView.java
+++ app/src/main/java/com/stergiadis/simplegallery/GridRecyclerView.java
@@ -2,12 +2,33 @@ package com.stergiadis.simplegallery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class GridRecyclerView extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    String[] testDataSet = {"one", "oni", "bum", "test", "test2", "fsdfb", "sfdg"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_recycler_view);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.grid_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new DataAdapter(testDataSet);
+        mRecyclerView.setAdapter(mAdapter);
+
+
     }
 }
