80e2c6ce8fcfd0a62f073dc8dce11a58b7116c8b
Steru <s.stergiadis@gmail.com>
2016-06-29 12:15:47 +0200

    Check if the folder isn't empty or there is no sdcard

--- app/src/main/java/com/stergiadis/simplegallery/GridRecyclerView.java
+++ app/src/main/java/com/stergiadis/simplegallery/GridRecyclerView.java
@@ -12,6 +12,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
+import com.stergiadis.simplegallery.model.Image;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,7 +21,7 @@ import java.util.List;
 
 public class GridRecyclerView extends AppCompatActivity {
 
-    private static final int MAX_NUMBER_OF_GRID_ELEMENTS = 30;
+    private static final int MAX_NUMBER_OF_GRID_ELEMENTS = 150;
     private static final String[] EXTENSIONS_STRING_TAB = {"jpg", "bmp", "png", "jpeg"};
     private static final int GRID_SPAN_COUNT = 3;
 
@@ -49,38 +51,50 @@ public class GridRecyclerView extends AppCompatActivity {
         mLayoutManager = new GridLayoutManager(getApplicationContext(), GRID_SPAN_COUNT);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        //todo zabezpieczenia w razie pustego folderu lub braku karty itp
+
         mFileList = new ArrayList<>();
-        getFiles(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath(),
-                     mFileList);
+
+        boolean cardIsPresent = android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+        if(cardIsPresent == false) {
+
+            Toast.makeText(getApplicationContext(), R.string.error_msg_no_sdcard_found, Toast.LENGTH_LONG);
+
+        } else {
+            getFiles(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath(),
+                    mFileList);
+
+            if (mFileList.isEmpty()) {
+                Toast.makeText(getApplicationContext(), R.string.error_msg_no_files_found, Toast.LENGTH_LONG);
+            } else {
 
 
-        //write filenames and paths from List<File> to List<Image>
-        mImageList = new ArrayList<>();
-        for(File f : mFileList){
-            Image img = new Image();
-            img.setName(f.getName());
-            img.setPath(f.getAbsolutePath());
-            mImageList.add(img);
-        }
+                //write filenames and paths from List<File> to List<Image>
+                mImageList = new ArrayList<>();
+                for (File f : mFileList) {
+                    Image img = new Image();
+                    img.setName(f.getName());
+                    img.setPath(f.getAbsolutePath());
+                    mImageList.add(img);
+                }
 
-        mImageSubList = new ArrayList<>();
-        if(savedInstanceState != null){
-            mImageSubList = savedInstanceState.getParcelableArrayList(GridRecyclerView.PARCELABLE_NAME_IMAGE_LIST);
-        } else { //only n files showing, user can shuffle those with swipe down (swipeRefresh)
-            if (mImageList.size() > MAX_NUMBER_OF_GRID_ELEMENTS) {
-                Collections.shuffle(mImageList);
-                mImageSubList = new ArrayList<>(mImageList.subList(0, MAX_NUMBER_OF_GRID_ELEMENTS));
+                mImageSubList = new ArrayList<>();
+                if (savedInstanceState != null) {
+                    mImageSubList = savedInstanceState.getParcelableArrayList(GridRecyclerView.PARCELABLE_NAME_IMAGE_LIST);
+                } else { //only n files showing, user can shuffle those with swipe down (swipeRefresh)
+                    if (mImageList.size() > MAX_NUMBER_OF_GRID_ELEMENTS) {
+                        Collections.shuffle(mImageList);
+                        mImageSubList = new ArrayList<>(mImageList.subList(0, MAX_NUMBER_OF_GRID_ELEMENTS));
+                    }
+                }
+
+                mAdapter = new DataAdapter(mImageSubList, getApplicationContext());
+                mRecyclerView.setAdapter(mAdapter);
+
+                setGridListeners();
+
+                setSwipeRefresh();
             }
         }
-
-        mAdapter = new DataAdapter(mImageSubList, getApplicationContext());
-        mRecyclerView.setAdapter(mAdapter);
-
-        setGridListeners();
-
-        setSwipeRefresh();
-
     }
 
     @Override
@@ -107,23 +121,16 @@ public class GridRecyclerView extends AppCompatActivity {
                 View child = rv.findChildViewUnder(e.getX(), e.getY());
                 if(child != null && gestureDetector.onTouchEvent(e)) {
                     int position = rv.getChildAdapterPosition(child);
-                    Toast.makeText(getApplicationContext(), mFileList.get(position).getName(), Toast.LENGTH_SHORT).show();
-
-//                    Bundle bundle = new Bundle();
-//                    bundle.putSerializable("fileList", mImageList);
 
                     if (mImageList.isEmpty()){
-                        Toast.makeText(getApplicationContext(), "mImageListIsEmpty", Toast.LENGTH_LONG);
+                        Toast.makeText(getApplicationContext(), R.string.error_msg_no_files_found , Toast.LENGTH_LONG);
                     }else {
                         Intent intent = new Intent(getApplicationContext(), FullscreenImageActivity.class);
                         intent.putParcelableArrayListExtra(PARCELABLE_NAME_IMAGE_LIST, mImageSubList);
                         intent.putExtra(PARCELABLE_NAME_POSITION, position);
                         startActivity(intent);
                     }
-                    //getArguments().getSerializable("fileList");
-
                 }
-
                 return false;
             }
 
@@ -152,7 +159,6 @@ public class GridRecyclerView extends AppCompatActivity {
                 mRecyclerView.setAdapter(mAdapter);
                 mSwipeRefreshLayout.setRefreshing(false);
             }
-
         });
 
     }
@@ -163,7 +169,6 @@ public class GridRecyclerView extends AppCompatActivity {
     private void getFiles(String dirName, List<File> fileList){
 
         File dir = new File(dirName);
-
 
         File[] fileTab = dir.listFiles();
 
@@ -191,23 +196,3 @@ public class GridRecyclerView extends AppCompatActivity {
     }
 }
 
-
-
-//        if (mFileDir.isDirectory()) {
-//            mFileList = mFileDir.listFiles();
-//
-//            mFileNameString = new String[mFileTab.length];
-//
-//            String tmp;
-//            for (int i = 0; i < mFileTab.length; i++) {
-//                mFileNameString[i] = mFileTab[i].getName();
-//
-//            }
-//
-//            tmp = mFileTab.length + "\n" + mFileNameString[1];
-//
-//            mTempTextView.setText(tmp);
-//
-//        } else {
-//            mTempTextView.setText("mFile is not a directory!" + mFileDir);
-//        }
